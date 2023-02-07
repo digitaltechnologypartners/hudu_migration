7bf0dc2a50a6a2329b82645333df6ba7d593540b
Travis Grillot <travis.grillot@dtpartners.com>
2023-02-07 09:49:49 -0500

    json

--- hudu_migrator.py
+++ hudu_migrator.py
@@ -62,7 +62,7 @@ def createcompanies():
     connection = getExportDB()
     organizations = pd.read_sql(query,con=connection)
     organizations = organizations.to_json(orient = 'records')
-    organizations = json.load(organizations)
+    organizations = json.dump(organizations)
     companies = []
 
     for org in organizations:
