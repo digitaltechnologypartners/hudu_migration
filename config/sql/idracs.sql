b65daba90ac64914630fa965fa560de0845a6821
Clayton Gilchrist <clayton.gilchrist@dtpartners.com>
2023-03-29 16:56:22 -0400

    Renamed cols and added archived and status cols

--- config/sql/idracs.sql
+++ config/sql/idracs.sql
@@ -1,7 +1,9 @@
-SELECT configurations.name AS 'Name',
-	configurations.organization AS 'Company',
-	configurations.location AS 'Location',
-	configurations.model AS 'Model',
+SELECT configurations.name,
+	configurations.organization AS company,
+	configurations.location,
+	configurations.configuration_status,
+	configurations.archived,
+	configurations.model,
 	CASE
 		WHEN JSON_EXTRACT(configurations.configuration_interfaces, '$[0].name') LIKE '%LAN%'
 			THEN JSON_EXTRACT(configurations.configuration_interfaces, '$[0].ip_address')
@@ -11,8 +13,8 @@ SELECT configurations.name AS 'Name',
 			THEN JSON_EXTRACT(configurations.configuration_interfaces, '$[1].ip_address')
 		WHEN (JSON_EXTRACT(configurations.configuration_interfaces, '$[1].ip_address') NOT LIKE '%192.168.10_.%') OR (JSON_EXTRACT(configurations.configuration_interfaces, '$[1].ip_address') LIKE '%192.168.100.%')
 			THEN JSON_EXTRACT(configurations.configuration_interfaces, '$[1].ip_address')
-	END AS 'Local IP',
-	'' AS 'License Key',
+	END AS local_ip,
+	'' AS license_key,
 	CASE
 		WHEN JSON_EXTRACT(configurations.configuration_interfaces, '$[0].name') LIKE '%NAT%'
 			THEN TRUE
@@ -23,7 +25,7 @@ SELECT configurations.name AS 'Name',
 		WHEN (JSON_EXTRACT(configurations.configuration_interfaces, '$[1].ip_address') LIKE '%192.168.10_.%') AND (JSON_EXTRACT(configurations.configuration_interfaces, '$[1].ip_address') NOT LIKE '%192.168.100.%')
 			THEN TRUE
 		ELSE FALSE
-	END AS 'Has Tunnel',
+	END AS has_tunnel,
 	CASE
 		WHEN JSON_EXTRACT(configurations.configuration_interfaces, '$[0].name') LIKE '%NAT%'
 			THEN JSON_EXTRACT(configurations.configuration_interfaces, '$[0].ip_address')
@@ -33,6 +35,6 @@ SELECT configurations.name AS 'Name',
 			THEN JSON_EXTRACT(configurations.configuration_interfaces, '$[1].ip_address')
 		WHEN (JSON_EXTRACT(configurations.configuration_interfaces, '$[1].ip_address') LIKE '%192.168.10_.%') AND (JSON_EXTRACT(configurations.configuration_interfaces, '$[1].ip_address') NOT LIKE '%192.168.100.%')
 			THEN JSON_EXTRACT(configurations.configuration_interfaces, '$[1].ip_address')
-	END AS 'Natted IP'
+	END AS natted_ip
 FROM mdb.configurations
 WHERE (configurations.name LIKE '%iDRAC%') OR (configurations.configuration_type LIKE '%iDRAC%');
